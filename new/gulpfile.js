3a75778438d8dc0f9068c05eb04735f144de6ffd
Fareez Ahmed <fareez1234@gmail.com>
2016-12-25 21:52:44 +1100

    Updated the Mina Website Content with PUG

--- new/gulpfile.js
+++ new/gulpfile.js
@@ -1,22 +1,5 @@
 var gulp = require('gulp');
-// var jade = require('gulp-jade');
 var pug = require('gulp-pug');
-
-//
-// gulp.task('jade', function() {
-//   gulp.src('./src/*.jade')
-//   .pipe(jade ({
-//     pretty: true
-//   }))
-//   .pipe(gulp.dest('./dist'))
-// });
-//
-//
-// gulp.task('watch', function () {
-//   gulp.watch('./src/*.jade', ['jade'])
-// });
-//
-// gulp.task('default', ['jade','watch']);
 
 gulp.task('pug', function() {
   gulp.src('./src/*.pug')
